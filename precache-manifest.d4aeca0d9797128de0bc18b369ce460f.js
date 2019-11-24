7626662f5c3730ee352df4dcd68039e65c32d908
Bizyaev Sergey <SpawN-BK@yandex.ru>
2019-11-24 12:39:53 +0400

    Updates

---
+++ precache-manifest.d4aeca0d9797128de0bc18b369ce460f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458dc71adec5bebb8ecb8b20b028c47c",
+    "url": "/vk-webcam-zks/index.html"
+  },
+  {
+    "revision": "407c57eb72f0b3b80300",
+    "url": "/vk-webcam-zks/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "5a7e732bae213460c825",
+    "url": "/vk-webcam-zks/static/css/main.2410e8a4.chunk.css"
+  },
+  {
+    "revision": "407c57eb72f0b3b80300",
+    "url": "/vk-webcam-zks/static/js/2.5e6d373d.chunk.js"
+  },
+  {
+    "revision": "5a7e732bae213460c825",
+    "url": "/vk-webcam-zks/static/js/main.535442d6.chunk.js"
+  },
+  {
+    "revision": "17b2b02165c4203faefd",
+    "url": "/vk-webcam-zks/static/js/runtime-main.a25f0d69.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-webcam-zks/static/media/persik.4e1ec840.png"
+  }
+]);
