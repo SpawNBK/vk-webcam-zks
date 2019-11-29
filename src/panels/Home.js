3a653b473942dc5edd98f396056fea56b2a475a2
Bizyaev Sergey <SpawN-BK@yandex.ru>
2019-11-29 13:04:40 +0400

    fix names

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -17,7 +17,7 @@ import '../css/bootstrap-grid.css';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Телесети: Городские камеры</PanelHeader>
+		<PanelHeader>Городские камеры "Жигулевск"</PanelHeader>
 		{fetchedUser &&
 		<Group title="Добро пожаловать">
 			<Cell
@@ -36,28 +36,28 @@ const Home = ({ id, go, fetchedUser }) => (
 				<div class="row">
 					<div class="col-md-6 col-xs-12">
 						<p class="cam-title">Площадь мира</p>
-						<iframe title="Площадь мира" allowfullscreen="allowfullscreen" src="http://188.190.159.29/ploshad.mira-2-e33ee19a97/embed.html?autoplay=false&token=0aeddf9d-407d-4325-9a3b-b89f133dcf1b"></iframe>
+						<iframe title="Площадь мира" allowfullscreen="allowfullscreen" src="https://cam.tks-net.ru/vsaas/embed/nag.omny_m54e_2812-aeebe9c095?autoplay=false&dvr=false&token=2.L9viw74hAAEABZhztBGlh7kArCw-eZfVJYGe8o9A_1PD6kQH"></iframe>
 						
 					</div>
 					<div class="col-md-6 col-xs-12">
 						<p class="cam-title">Площадь мира 2</p>
-						<iframe title="Площадь мира 2" allowfullscreen="allowfullscreen" src="http://188.190.159.29/nag.omny_m54e_2812-aeebe9c095/embed.html?autoplay=false&token=4c54eef9-c5f1-419a-81fe-e01826ed054c"></iframe>
+						<iframe title="Площадь мира 2" allowfullscreen="allowfullscreen" src="https://cam.tks-net.ru/vsaas/embed/ploshad.mira-2-e33ee19a97?autoplay=false&dvr=false&token=2.L9viw74hAAEABZhztBGlh3vTqIz-VmuhrYUKlt_tEfssAMfZ"></iframe>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-6 col-xs-12">
 						<p class="cam-title">Набережная</p>
-						<iframe title="Набережная" allowfullscreen="allowfullscreen" src="http://188.190.159.29/dahua.dh-sd59225u-hni-0001f522af/embed.html?autoplay=false&token=8b623099-5d81-4baa-88d7-513adac3061a"></iframe>
+						<iframe title="Набережная" allowfullscreen="allowfullscreen" src="https://cam.tks-net.ru/vsaas/embed/dahua.dh-sd59225u-hni-0001f522af?autoplay=false&dvr=false&token=2.L9viw74hAAEABZhztBGlh40_6rbxdHKDWdlR6UjbVoEaExVW"></iframe>
 					</div>
 					<div class="col-md-6 col-xs-12">
 						<p class="cam-title">Площадь победы</p>
-						<iframe title="Площадь победы" allowfullscreen="allowfullscreen" src="http://188.190.159.29/nag.omny_m54e_2812-24f0aefa23/embed.html?autoplay=false&token=25844a37-4c5c-45c4-90ca-ad322a561db1"></iframe>
+						<iframe title="Площадь победы" allowfullscreen="allowfullscreen" src="https://cam.tks-net.ru/vsaas/embed/nag.omny_m54e_2812-24f0aefa23?autoplay=false&dvr=false&token=2.L9viw74hAAEABZhztBGlhz0T_9z4Y2kIb_y-oNB2Zl5ToYA_"></iframe>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-6 col-xs-12">
 						<p class="cam-title">Сквер В-1</p>
-						<iframe title="Сквер В-1" allowfullscreen="allowfullscreen" src="http://188.190.159.29/nag.omny_m54e_2812-41e047cbf6/embed.html?autoplay=false&token=142f5d16-d968-4f5d-91d2-3126ea8fa272"></iframe>
+						<iframe title="Сквер В-1" allowfullscreen="allowfullscreen" src="https://cam.tks-net.ru/vsaas/embed/nag.omny_m54e_2812-41e047cbf6?autoplay=false&dvr=false&token=2.L9viw74hAAEABZhztBGlh6eUI2CMkev5in-R-s_O0cTsbtII"></iframe>
 					</div>
 					<div class="col-md-6 col-xs-12">
 						
