c414a61f6d9b094affd6e1b3ee234eb1c4ae3467
Bizyaev Sergey <SpawN-BK@yandex.ru>
2019-11-29 15:32:49 +0400

    Updates

--- precache-manifest.0e5dfd3b9eddccc73d8dcec4cbcdd949.js
+++ precache-manifest.0e5dfd3b9eddccc73d8dcec4cbcdd949.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0bae91750144501a3562b2f7065894cd",
+    "revision": "a976fc31310461a981e2cb5fdaa105e9",
     "url": "/vk-webcam-zks/index.html"
   },
   {
-    "revision": "407c57eb72f0b3b80300",
+    "revision": "2a3f649251af8093f882",
     "url": "/vk-webcam-zks/static/css/2.7214840c.chunk.css"
   },
   {
-    "revision": "71124ad041b7532378b3",
-    "url": "/vk-webcam-zks/static/css/main.2410e8a4.chunk.css"
+    "revision": "287a1b6af955089d8bed",
+    "url": "/vk-webcam-zks/static/css/main.a8493749.chunk.css"
   },
   {
-    "revision": "407c57eb72f0b3b80300",
-    "url": "/vk-webcam-zks/static/js/2.5e6d373d.chunk.js"
+    "revision": "2a3f649251af8093f882",
+    "url": "/vk-webcam-zks/static/js/2.cc43642e.chunk.js"
   },
   {
-    "revision": "71124ad041b7532378b3",
-    "url": "/vk-webcam-zks/static/js/main.a0d689de.chunk.js"
+    "revision": "287a1b6af955089d8bed",
+    "url": "/vk-webcam-zks/static/js/main.c22e0a2a.chunk.js"
   },
   {
     "revision": "17b2b02165c4203faefd",
